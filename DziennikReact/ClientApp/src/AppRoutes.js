db90f41165901e520a896d3ade371cbe5b121a44
Pele <banskimateusz7@gmail.com>
2023-11-24 02:25:28 +0100

    Delete unused files

--- DziennikReact/ClientApp/src/AppRoutes.js
+++ DziennikReact/ClientApp/src/AppRoutes.js
@@ -1,28 +1,8 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
-import { Test } from "./components/Test";
 import { Dziennik } from "./components/Dziennik";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  },
-  {
-    path: '/test',
-    element: <Test />
-  },
-  {
-    path: '/dziennik',
     element: <Dziennik />
   }
 ];
--- DziennikReact/ClientApp/src/components/Layout.js
+++ DziennikReact/ClientApp/src/components/Layout.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -8,8 +7,7 @@ export class Layout extends Component {
   render() {
     return (
       <div>
-        <NavMenu />
-        <Container tag="main">
+        <Container className="mt-5" tag="main">
           {this.props.children}
         </Container>
       </div>
